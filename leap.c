86e89daaa49965775e72af0e26075f091a8e9781
Kaid02 <kabirS317@gmail.com>
2016-10-19 12:04:27 +0545

    Add files via upload

---
+++ leap.c
@@ -0,0 +1,26 @@
+/*
+ Student Name : Kabir D Shrestha
+ Subject : Programming Fundamental
+ Lab No. : 07
+ Program : Write a C program to print leap year
+ Date : 2016-10-19
+  */
+  #include<stdio.h>
+  int main(){
+	  int day;
+	  printf("Enter the year is leap year or not");
+	  printf("Enter a day:");
+	  scanf("%d",&day);
+	  
+	  if(day==29){
+		  printf("The year is leap year");
+	  }
+	  else {
+		  printf("The year is not leap year");
+	  }
+	  return 0;
+  }
+	  
+	  
+	  
+  
